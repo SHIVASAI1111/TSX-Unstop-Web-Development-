0d6f4acd189d7b5256716fa7049791e27a45cde9
SHIVASAI1111 <marojushivasai1@gmail.com>
2025-06-05 11:54:07 +0530

    Web Development
    A Portfolio Web Application Using HTML, CSS, JavaScript, and Bootstrap

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-console.log("Page has loaded!");
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
-function showMessage() {
-    console.log("Button was clicked!");
-    alert("Hello! This is your first JavaScript interaction.");
-}
+// Form submission
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Get form values
+    const name = this.elements[0].value;
+    const email = this.elements[1].value;
+    const subject = this.elements[2].value;
+    const message = this.elements[3].value;
+    
+    // Here you would typically send the data to a server
+    console.log({name, email, subject, message});
+    
+    // Show success message
+    alert('Thank you for your message! I will get back to you soon.');
+    
+    // Reset form
+    this.reset();
+});
+
+// Change navbar style on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
